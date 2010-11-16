45b318880aaf593efa0ad6d62a200374ae035f22
Mikael Roos <mos@bth.se>
2010-11-16 16:44:49 +0100

    preparing to tag new development version, adding admin interface and install procedure

---
+++ medes/data/default/header.php
@@ -0,0 +1,16 @@
+<!-- Top header with logo and navigation -->
+<header id="top">
+
+	<!-- Default phpmedes-logo -->
+	<div class=logo>
+		<p id=label>phpmedes
+		<p id=tagline>dbwebb.se
+	</div>
+
+	<!-- Use an image as logo -->
+	<!-- <img src="img/logo.png" alt="Logo" width=200 height=100> -->
+	
+</header>
+
+<!-- Top navigation bar -->
+<?php echo $GLOBALS['pp']->GetHTMLForNavbar(); ?>
