9362fa114660c57969098e28db37a8b33c41f5d2
Mikael Roos <mos@bth.se>
2010-11-16 16:38:20 +0100

    preparing to tag new development version, adding admin interface and install procedure

--- medes/src/CInstall/home.php
+++ medes/src/CInstall/home.php
@@ -9,6 +9,80 @@ EOD;
 
 $check = "";
 
+// ------------------------------------------------------------------------------
+//
+// Create default settings for configuration
+//
+$config['header'] = <<<'EOD'
+<!-- Top header with logo and navigation -->
+<header id="top">
+
+	<!-- Default phpmedes-logo -->
+	<div class=logo>
+		<p id=label>phpmedes
+		<p id=tagline>dbwebb.se
+	</div>
+
+	<!-- Use an image as logo -->
+	<!-- <img src="img/logo.png" alt="Logo" width=200 height=100> -->
+	
+</header>
+
+<!-- Top navigation bar -->
+<?php echo $GLOBALS['pp']->GetHTMLForNavbar(); ?>
+
+EOD;
+
+$config['footer'] = <<<'EOD'
+<?php $pp = CPrinceOfPersia::GetInstance(); ?>
+
+<footer id="bottom">
+
+	<nav>
+		<h5>phpmedes</h5>
+			<a href='http://phpmedes.org/'>phpmedes.org</a>
+	</nav>
+	
+	<nav>
+		<h5>Tools</h5>
+		<a href="http://validator.w3.org/check/referer">HTML5</a> 
+		<a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">CSS3</a>
+		<a href="http://validator.w3.org/unicorn/check?ucn_uri=referer&#38;ucn_task=conformance">Unicorn</a>
+		<a href="http://www.w3.org/2009/cheatsheet/">Cheatsheet</a>
+		<a href="http://validator.w3.org/checklink?uri=<?php echo $pp->GetUrlToCurrentPage(); ?>">Link Checker</a>
+		<a href="http://qa-dev.w3.org/i18n-checker/index?async=false&#38;docAddr=<?php echo $pp->GetUrlToCurrentPage(); ?>">i18n Checker</a>
+		<a href="http://web-sniffer.net/?url=<?php echo $pp->GetUrlToCurrentPage(); ?>">Check header</a>
+	</nav>
+	
+	<nav>
+		<h5>Manuals</h5>
+		<a href="http://dev.w3.org/html5/spec/spec.html">HTML5</a> 
+		<a href="http://www.w3.org/TR/CSS2/">CSS2</a> 
+		<a href="http://www.w3.org/Style/CSS/current-work#CSS3">CSS3</a> 
+		<a href="http://php.net/manual/en/index.php">PHP</a> 
+		<a href="http://www.sqlite.org/lang.html">SQL (SQLite)</a>
+	</nav>
+
+	<p class=license>
+	The content on this site is licensed according to  
+	<a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution Share-Alike License v3.0</a>
+	or any later version. The software phpmedes is free software, open source, licensed according to 
+	<a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License version 3</a>.
+	</p>
+	
+	<p class=medes>This site is built using <a href="http://phpmedes.org/">the free and opensource software named phpmedes</a>.</p>
+
+</footer>
+
+EOD;
+
+if(!isset($pp->config['header'])) {
+	$pp->config['header'] = config['header'];
+}
+
+$config['stylesheet'] = 'stylesheet_compatibility.css'
+
+
 
 // ------------------------------------------------------------------------------
 //
