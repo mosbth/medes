be67ff81f4b89e6ee6f15cf8034e51b40dbec233
Mikael Roos <mos@bth.se>
2010-11-29 15:45:02 +0100

    enabling custom merge process

--- medes/merge.php
+++ medes/merge.php
@@ -3,8 +3,16 @@ require_once("config.php");
 
 $config = unserialize(file_get_contents($this->medesPath . "/data/{$className}_config_backup.php"));
 
+$pp->config['header'] = $config['header'];
+$pp->config['footer'] = $config['footer'];
+$pp->config['password'] = $config['password'];
+
 $pp->config['navigation']['navbar'] = array(
 	"text"=>"Main navigation",
 	"nav"=>$config['navbar'];
+);
+$pp->config['navigation']['relatedsites'] = array(
+	"text"=>"Top left menu",
+	"nav"=>$config['relatedsites'];
 );
 $pp->Dump;
