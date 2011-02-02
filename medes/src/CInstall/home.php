bb961ff0db1db96b07c496c0ab0efd9cc229e190
Mikael Roos <mos@bth.se>
2011-02-02 16:22:16 +0100

    updates while installing a live site

--- medes/src/CInstall/home.php
+++ medes/src/CInstall/home.php
@@ -185,7 +185,7 @@ EOD;
 // Set $page to contain html for the page
 //
 $page = <<<EOD
-<article id="install" class="colborder span-18 last">
+<article id="install" class="container colborder span-18 last">
 <h1>Do a fresh installation of medes</h1>
 <hr>
 <!-- <h1>Do a fresh (re-)installation of medes</h1> -->
