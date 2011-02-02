7b3a7dab316cca383c71529ea9fe679785fdcfbb
Mikael Roos <mos@bth.se>
2011-02-02 16:37:14 +0100

    updates while installing a live site

--- medes/src/CInstall/default_site_footer.php
+++ medes/src/CInstall/default_site_footer.php
@@ -11,14 +11,16 @@ $html = <<<EOD
 <footer id="bottom-wrap">
 <footer id="bottom" class="container">
 
+<!--
 	<p class="span-24 last">The content on this site is licensed according to  
 	<a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution Share-Alike License v3.0</a>
-	or any later version. The software phpmedes is free software, open source, licensed according to 
-	<a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License version 3</a>.
+	or any later version. 
 	</p>
-	
+-->
+
 	<p class="span-24 last">This site is built using <a href="http://phpmedes.org/">the free 
-	and opensource software named phpmedes</a>.
+	and opensource software named phpmedes</a>. PhpMedes is free software, open source, and therefore licensed 
+	according to <a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License version 3</a>.
 	</p>
 	
 	{$developer}
