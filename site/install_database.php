6fed1bf2764224dcd3e1f48553b6140f99e77019
Mikael Roos <mos@bth.se>
2011-07-04 16:39:12 +0200

    preparing to tag new medes

--- site/install_database.php
+++ site/install_database.php
@@ -3,7 +3,7 @@
 // Use Medes bootstrap to gain access to fully populated $pp
 define('MEDES_FRONTCONTROLLER_PASS', true);
 define('MEDES_TEMPLATEENGINE_PASS', true);
-include(__DIR__ . "/index.php");
+include(__DIR__ . "/../index.php");
 
 // For all classes, check if module IInstallable, call method for install
 echo "hej";
