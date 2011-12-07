89b67db4e3b82082db296f0220e1f1a9e0056c82
Mikael Roos <mikael.t.h.roos@gmail.com>
2011-12-07 12:53:29 +0100

    making CRequest to leaf-class, removed dependency to pp. Support for installing in subdirectory and modify baseurl.

--- src/CCtrl4Error/CCtrl4Error.php
+++ src/CCtrl4Error/CCtrl4Error.php
@@ -23,6 +23,17 @@ class CCtrl4Error implements IController {
 		$feedback	= $pp->GetHTMLForFeedback();
 		$pp->AddView(new CView("<h1>Error Controller</h1>{$feedback}<p>404</p>"));
 		$pp->AddView(new CView(array('html'=>'<h2>$pp->req</h2><pre>' . print_r($pp->req, true) . '</pre>')));
+
+  	$url 		= $pp->req->GetUrlToCurrentPage();
+  	$parts 	= parse_url($url);
+  	$script	= $_SERVER['SCRIPT_NAME'];
+ 		$dir 		= rtrim(dirname($script), '/');
+ 		$query	= substr($parts['path'], strlen($dir));
+ 		$splits = explode('/', trim($query, '/'));
+
+    echo "$url<br>";
+    echo "$script<br>";
+    echo "$dir<br>";
 	}
 
 
