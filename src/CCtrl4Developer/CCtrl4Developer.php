85dadd5a128a29654e87c20850281c833ca040e0
Mikael Roos <mikael.t.h.roos@gmail.com>
2011-12-07 14:43:29 +0100

    Enable to send url-parts as arguments to a controller method

--- src/CCtrl4Developer/CCtrl4Developer.php
+++ src/CCtrl4Developer/CCtrl4Developer.php
@@ -7,43 +7,20 @@
 class CCtrl4Developer implements IController {
 
 	/**
-	 * A reference to current CPrinceOfPersia
-   * @var CPrinceOfPersia
-   */
-	private $pp;
-	
-	/**
 	 * Constructor
 	 */
 	public function __construct() {
-		$this->pp = CPrinceOfPersia::GetInstance();
-		$pp = &$this->pp;
-		
 		$header = '<h1>Developer information</h2><p>Here is useful information for a developer, and maybe e menu (bar) of the options.</p>';
 
 		$nav = array(
-			array(
-				'text'=>'Configuration',
-				'href'=>$pp->req->CreateUrlToControllerAction(null, 'config'),
-			),
-			array(
-				'text'=>'Request',
-				'href'=>$pp->req->CreateUrlToControllerAction(null, 'request'),
-			),
-			array(
-				'text'=>'$_SERVER',
-				'href'=>$pp->req->CreateUrlToControllerAction(null, 'server'),
-			),
-			array(
-				'text'=>'$_SESSION',
-				'href'=>$pp->req->CreateUrlToControllerAction(null, 'session'),
-			),
+			array('text'=>'Configuration', 'href'=>$pp->req->CreateUrlToControllerAction(null, 'config'),),
+			array('text'=>'Request', 'href'=>$pp->req->CreateUrlToControllerAction(null, 'request'),),
+			array('text'=>'$_SERVER', 'href'=>$pp->req->CreateUrlToControllerAction(null, 'server'),),
+			array('text'=>'$_SESSION', 'href'=>$pp->req->CreateUrlToControllerAction(null, 'session'),),
+			array('text'=>'Call controller method using arguments', 'href'=>$pp->req->CreateUrlToControllerAction(null, 'args'),),
 		);
 		$header .= CNavigation::GenerateMenu($nav, $pp->pageUseListForMenus, 'mds-nav-developer', 'mds-nav-developer mds-nav-tabs');	
-
-		$v = new CView();
-		$v->AddStatic($header);
-		$pp->AddView($v, -1);
+		$pp->AddView(new CView($header);
 	}
 	
 	
@@ -58,7 +35,7 @@ class CCtrl4Developer implements IController {
  	 * Action to print out the $pp cfg (configuration).
 	 */
 	public function Config() {	
-		$pp = &$this->pp;
+		global $pp;
 		$pp->AddView(new CView(array('html'=>'<h2>$pp->cfg</h2><pre>' . print_r($pp->cfg, true) . '</pre>')));
 	}
 
@@ -67,7 +44,7 @@ class CCtrl4Developer implements IController {
  	 * Action to print out the request object.
 	 */
 	public function Request() {	
-		$pp = &$this->pp;
+		global $pp;
 		$pp->AddView(new CView(array('html'=>'<h2>$pp->req</h2><pre>' . print_r($pp->req, true) . '</pre>')));
 	}
 
@@ -76,7 +53,7 @@ class CCtrl4Developer implements IController {
  	 * Action to print out $_SERVER.
 	 */
 	public function Server() {	
-		$pp = &$this->pp;
+		global $pp;
 		$pp->AddView(new CView(array('html'=>'<h2>$_SERVER[]</h2><pre>' . print_r($_SERVER, true) . '</pre>')));
 	}
 
@@ -85,8 +62,25 @@ class CCtrl4Developer implements IController {
  	 * Action to print out $_SESSION.
 	 */
 	public function Session() {	
-		$pp = &$this->pp;
+		global $pp;
 		$pp->AddView(new CView(array('html'=>'<h2>$_SESSION[]</h2><pre>' . print_r($_SESSION, true) . '</pre>')));
+	}
+
+
+	/**
+ 	 * Action to take pass arguments to controller method through the url
+	 */
+	public function Args($v1=null, $v2=null, $v3=null) {	
+		global $pp;
+		$html = "<h2>Passing urlparts as methid arguments</h2><p>This controller method takes three arguments that can be passed through the url.</p>";
+		$html .= "<a href='" . $pp->req->CreateUrlToControllerAction(null, 'args', 1) . "'>Passing one argument</a> | ";
+		$html .= "<a href='" . $pp->req->CreateUrlToControllerAction(null, 'args', 1, 2) . "'>Passing two arguments</a> | ";
+		$html .= "<a href='" . $pp->req->CreateUrlToControllerAction(null, 'args', 1, 2, 3) . "'>Passing three arguments</a>";
+		$html .= "<pre>Argument 1 = " . (is_int($v1) ? $v1 : 'is not a number.');
+		$html .= "Argument 2 = " . (is_int($v2) ? $v2 : 'is not a number.');
+		$html .= "Argument 3 = " . (is_int($v3) ? $v3 : 'is not a number.');
+		$html .= "</pre>";
+		$pp->AddView(new CView($html));
 	}
 
 
